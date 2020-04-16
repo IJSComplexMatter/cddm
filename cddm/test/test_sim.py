e74f81a86863c2ac867df032ec78c48aa39401cc
andrej5elin <andrej.petelin@ijs.si>
2020-04-16 19:07:01 +0200

    Create test_sim.py

---
+++ cddm/test/test_sim.py
@@ -0,0 +1,25 @@
+"""tests for Brownian simulator"""
+
+import unittest
+
+from cddm.sim import plot_random_walk, create_random_times1,create_random_times2,\
+     simple_brownian_video     , seed
+
+class TestSim(unittest.TestCase):
+    """Tests function validity"""
+    def setUp(self):
+        seed(0)
+        
+    def test_plot(self):
+        plot_random_walk()
+        
+    def test_video(self):
+        video = simple_brownian_video(range(10))
+    
+    def test_dual_video(self):
+        t1,t2 = create_random_times1(32,2)
+        t1,t2 = create_random_times2(32,2)
+        video = simple_brownian_video(t1,t2)
+        
+if __name__ == "__main__":
+    unittest.main()
