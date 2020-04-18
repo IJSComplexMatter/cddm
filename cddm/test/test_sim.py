91e109295583dc1ea00563443e7b51df9467f08c
andrej5elin <andrej.petelin@ijs.si>
2020-04-18 21:25:16 +0200

    Update test_sim.py
    added several tests

--- cddm/test/test_sim.py
+++ cddm/test/test_sim.py
@@ -3,18 +3,35 @@
 import unittest
 
 from cddm.sim import plot_random_walk, create_random_times1,create_random_times2,\
-     simple_brownian_video     , seed
+     simple_brownian_video   , seed, data_trigger, mirror, brownian_walk
 
 class TestSim(unittest.TestCase):
     """Tests function validity"""
     def setUp(self):
         seed(0)
         
+    def test_mirror(self):
+        self.assertAlmostEqual(mirror(2.,0.,2.), 0.)
+        self.assertAlmostEqual(mirror(2.1,0.,2.), 0.1)
+        self.assertAlmostEqual(mirror(-0.1,0.,1.), 0.9)
+        self.assertAlmostEqual(mirror(0.,0.,1.), 0.)
+        
+    def test_brownian_walk(self):
+        with self.assertRaises(ValueError):
+            list(brownian_walk(((0,1),(2,3)),shape = (3,)))
+        with self.assertRaises(ValueError):
+            list(brownian_walk((0,1)))
+            
+    def test_data_trigger(self):
+        data = range(10)
+        indices = [1,4,7]
+        self.assertEqual(indices, [x for x in data_trigger(data, indices)])
+        
     def test_plot(self):
         plot_random_walk()
         
     def test_video(self):
-        video = simple_brownian_video(range(10))
+        video = simple_brownian_video(range(10), sigma = None, noise = 3.)
         for frames in video:
             pass
     
