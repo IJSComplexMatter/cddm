30e860a4d6f37da1518b3814b1a67cfbbe4e171d
andrej5elin <andrej.petelin@ijs.si>
2020-04-18 20:50:40 +0200

    Update test_sim.py

--- cddm/test/test_sim.py
+++ cddm/test/test_sim.py
@@ -15,11 +15,15 @@ class TestSim(unittest.TestCase):
         
     def test_video(self):
         video = simple_brownian_video(range(10))
+        for frames in video:
+            pass
     
     def test_dual_video(self):
         t1,t2 = create_random_times1(32,2)
         t1,t2 = create_random_times2(32,2)
         video = simple_brownian_video(t1,t2)
+        for frames in video:
+            pass
         
 if __name__ == "__main__":
     unittest.main()
