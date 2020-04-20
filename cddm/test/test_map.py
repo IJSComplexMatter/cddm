bda4c2bc6249e9b7d7bc97db803511b08a192d42
andrej5elin <andrej.petelin@ijs.si>
2020-04-20 20:44:47 +0200

    Create test_map.py

---
+++ cddm/test/test_map.py
@@ -0,0 +1,17 @@
+"""tests for map module"""
+
+import unittest
+
+from cddm.map import plot_indexmap, k_indexmap
+class Test(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_plot(self):
+        kmap = k_indexmap(33,32, angle = 0, sector = 5, kstep = 1.)
+        plot_indexmap(kmap)
+
+        
+if __name__ == "__main__":
+    unittest.main()
